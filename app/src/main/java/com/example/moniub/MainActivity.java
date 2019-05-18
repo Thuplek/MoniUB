b5fabd9b4299aaf19d648cb8927b6fce7d2d10f9
Thuplek <fellipewatanabecarneiro@gmail.com>
2019-05-17 21:27:16 -0300

    refatore code

---
+++ app/src/main/java/com/example/moniub/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.moniub;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // chamando tela de registro assim q o app abre so pra testar
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+
+    }
+}
