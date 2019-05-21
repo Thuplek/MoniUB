0ebae7fa1de0f0390712b5d471e185ba9215e380
Thuplek <fellipewatanabecarneiro@gmail.com>
2019-05-21 16:21:33 -0300

    bug-fix tela de login

--- app/src/main/java/com/example/moniub/LoginActivity.java
+++ app/src/main/java/com/example/moniub/LoginActivity.java
@@ -5,26 +5,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button cadastrar;
+    EditText matricula;
+    EditText senha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button entrar = findViewById(R.id.cadastrar);
-        entrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                registrar();
-                //
-            }
-        });
+
     }
-    public void registrar(){
-        Intent intent = new Intent(this, RegisterActivity.class);
+
+    public void entrar(View v) {
+        matricula = findViewById(R.id.numeroMatricula);
+        String sMatricula = matricula.getText().toString();
+        senha = findViewById(R.id.senha);
+        String sSenha = senha.getText().toString();
+        if(sMatricula.equals("12345") && sSenha.equals("12345")){
+
+        }else{
+            Toast.makeText(getApplicationContext(),"Credenciais incorretas",Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+
+
+    }
+
+    public void telaregister(View v) {
+        Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
         startActivity(intent);
     }
+
 }
