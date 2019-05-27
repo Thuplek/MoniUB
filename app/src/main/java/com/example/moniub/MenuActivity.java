0825625788d3fbbfa28346ca27b20150b8f008bd
Thuplek <fellipewatanabecarneiro@gmail.com>
2019-05-27 20:42:03 -0300

    Button sair criado e funcionando

--- app/src/main/java/com/example/moniub/MenuActivity.java
+++ app/src/main/java/com/example/moniub/MenuActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
@@ -13,24 +14,32 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        ImageView sair = findViewById(R.id.sair);
+
+        sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sair();
+            }
+        });
     }
     public void telaprincipal(View v) {
         Intent intent = new Intent(this,PrincipalActivity.class);
         startActivity(intent);
     }
     public void telaeditar(View v) {
-        //Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
-       // startActivity(intent);
+
         Toast.makeText(getApplicationContext(),"Em Breve",Toast.LENGTH_SHORT).show();
 
     }
     public void telamonitor(View v) {
-        //Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
-        // startActivity(intent);
+
         Toast.makeText(getApplicationContext(),"Em Breve",Toast.LENGTH_SHORT).show();
     }
     public void sair(){
-        finishAffinity();
+        finish();
+        moveTaskToBack(true);
     }
 
 }
